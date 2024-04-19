bf8a340616c838105495eb6ba83b34621562dab6
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-04-19 14:53:41 +0900

    장바구니에 있는 품목을 주문, 주문 후 장바구니 비우기

--- src/main/java/com/koreait/exam/batch_ex_24_04/BatchEx2404Application.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/BatchEx2404Application.java
@@ -1,4 +1,5 @@
 package com.koreait.exam.batch_ex_24_04;
+
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
